cf01f6700d706e72edbd734444a20fc1b927213a
Ofek Lev <ofekmeister@gmail.com>
2022-08-11 23:17:28 -0400

    Use Hatchling and allow direct references in dependencies

--- src/rsa_api/__init__.py
+++ src/rsa_api/__init__.py
@@ -1,6 +1,3 @@
-"""NTIA/ITS Python wrapper for the Tektronix RSA API for Linux
-
-Refer to the README file for detailed usage information.
-"""
-__version__ = "1.2.3"
 from .rsa_api import *
+
+__version__ = "1.2.3"
