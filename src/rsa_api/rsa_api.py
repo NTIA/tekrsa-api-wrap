a1545c02c70a130a245da4e694e68bcbc1bb4280
Doug Boulware <dboulware@ntia.doc.gov>
2024-04-29 17:39:21 -0600

    debugging

--- src/rsa_api/rsa_api.py
+++ src/rsa_api/rsa_api.py
@@ -1476,9 +1476,9 @@ class RSA:
         try:
             logger.debug(f"IQStreamIQInfo.timestamp: {iqinfo.timestamp}")
             logger.debug(f"IQStreamIQInfo.triggerCount: {iqinfo.triggerCount}")
-            logger.debug(f"IQStreamIQInfo.triggerIndices: {iqinfo.triggerIndices}")
-            logger.debug(f"IQStreamIQInfo.timestamp: {iqinfo.scaleFactor}")
-            logger.debug(f"IQStreamIQInfo.timestamp: {iqinfo.acqStatus}")
+            logger.debug(f"IQStreamIQInfo.triggerIndices: {iqinfo.triggerIndices.contents}")
+            logger.debug(f"IQStreamIQInfo.scaleFactor: {iqinfo.scaleFactor}")
+            logger.debug(f"IQStreamIQInfo.acqStatus: {iqinfo.acqStatus}")
         except Exception as ex:
             logger.debug(f"unable to log IQStreamIQInfo: {ex}")
         del c_iqdata
